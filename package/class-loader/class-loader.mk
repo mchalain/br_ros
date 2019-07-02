a04abd339ad76480f302a376deae8adaed11ec5f
mchalain <marc.chalain@smile.fr>
2019-07-02 15:46:29 +0200

    [class-loader] version 0.4.1 for ROS 1

--- package/class-loader/class-loader.mk
+++ package/class-loader/class-loader.mk
@@ -3,14 +3,14 @@
 # class_loader
 #
 ################################################################################
-CLASS_LOADER_VERSION = 1.3.1
+CLASS_LOADER_VERSION = 0.4.1
 CLASS_LOADER_SOURCE = $(CLASS_LOADER_VERSION).tar.gz
 CLASS_LOADER_SITE = https://github.com/ros/class_loader/archive
 CLASS_LOADER_LICENSE = BSD3
 CLASS_LOADER_LICENSE_FILES = LICENSE
 CLASS_LOADER_INSTALL_STAGING=YES
-CLASS_LOADER_DEPENDENCIES+=ament_cmake
-
-ROS_INSTALL_PREFIX=/opt/ros
+CLASS_LOADER_DEPENDENCIES+=host-cmake-modules
+CLASS_LOADER_DEPENDENCIES+=console-bridge
+CLASS_LOADER_DEPENDENCIES+=poco
 
 $(eval $(catkin-package))
