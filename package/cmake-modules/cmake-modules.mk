840bf4bb60aa7726d0c7591324c7c67b4589b56c
mchalain <marc.chalain@smile.fr>
2019-07-02 14:56:21 +0200

    [cmake_modules] build rules for host

--- package/cmake-modules/cmake-modules.mk
+++ package/cmake-modules/cmake-modules.mk
@@ -10,8 +10,6 @@ CMAKE_MODULES_LICENSE = BSD3
 CMAKE_MODULES_LICENSE_FILES = LICENSE
 CMAKE_MODULES_DEPENDENCIES+=host-catkin
 
-ROS_INSTALL_PREFIX=/opt/ros
-
 define CMAKE_MODULES_COPY_CMAKE
         cp -r $(HOST_DIR)/share/cmake_modules/cmake/* $(HOST_DIR)/share/catkin/cmake
         cp -r $(HOST_DIR)/share/cmake_modules/cmake/Modules/* $(HOST_DIR)/share/cmake-$(CMAKE_VERSION_MAJOR)/Modules/
@@ -19,5 +17,4 @@ endef
 
 HOST_CMAKE_MODULES_POST_INSTALL_HOOKS += CMAKE_MODULES_COPY_CMAKE
 
-$(eval $(catkin-package))
 $(eval $(host-catkin-package))
