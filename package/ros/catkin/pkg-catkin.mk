2f15869557c5d3c142486ef074db0c8398bda0fb
mchalain <marc.chalain@gmail.com>
2019-07-26 16:16:38 +0200

    [catkin] python scripts installation on target

--- package/ros/catkin/pkg-catkin.mk
+++ package/ros/catkin/pkg-catkin.mk
@@ -15,6 +15,16 @@ $(2)_CONF_OPTS += -DCATKIN_ENABLE_TESTING=0
 $(2)_CONF_OPTS += -DPYTHON_VERSION=$(PYTHON_VERSION)
 #$(2)_CONF_OPTS += -DCMAKE_FIND_DEBUG_MODE=ON
 
+define $(2)_CATKIN_INSTALL_ROSDEVEL
+## this part was replaced by a patch into python_destutils_install.sh.in
+#	find $(TARGET_DIR)$(BR2_PACKAGE_ROS_INSTALL_PREFIX)/bin/* | xargs --no-run-if-empty \
+#		$(SED)  "s,$$(HOST_DIR),@HOSTDIR@,g" \
+#				-e "s,@HOSTDIR@,/usr,g"
+	$$(if $$(wildcard $$(@D)$$($(2)_SUBDIR:%=/%)/devel/lib), cp -r $$(@D)$$($(2)_SUBDIR:%=/%)/devel/lib/* $(TARGET_DIR)$(BR2_PACKAGE_ROS_INSTALL_PREFIX)/lib)
+endef
+
+$(2)_POST_INSTALL_TARGET_HOOKS += $(2)_CATKIN_INSTALL_ROSDEVEL
+
 $(call inner-cmake-package,$(1),$(2),$(3),$(4))
 
 endef
